05d6de844f3b5791df392762a2ddc18a8da3a33d
Pavel Prisecariu <prisecariupavel22@stud.ase.ro>
2023-11-26 00:05:48 +0200

    Defined the Movie class and its attributes

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Ticket {
@@ -23,6 +24,28 @@ public:
 		return date;
 	}
 
+};
+
+/*class EventLocation {
+	//1int max_no_of_seats;
+	int no_of_rows;
+	int* no_of_seats;
+
+};*/
+
+class Movie {
+	char* title;
+	string date;
+	string time;
+	int duration;
+	char* genre;
+	string format;
+
+	//accessors
+	//char* getTitle() {
+		//if(this->title)
+	//}
+	//string lastFour = myString.substr(myString.length() - 4);
 };
 
 int main()
