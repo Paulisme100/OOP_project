2c35ab0569b4dd195b75aa74a20e587a9b42cf94
Pavel Prisecariu <prisecariupavel22@stud.ase.ro>
2023-11-26 21:29:22 +0200

    Added second generic method to class EventLocation, overloaded the operator = and did the overload for operator -- and operator !

--- Project1/EventLocation.h
+++ Project1/EventLocation.h
@@ -7,12 +7,21 @@
 
 class EventLocation {
 
+	char* locationName;
 	int no_of_rows;
 	int* no_of_seats;
 	bool hasConcessionStand;
 
 public:
 	//accessors
+	char* getLocName() {
+		if (this->locationName != NULL)
+		{
+			char* copy = new char[strlen(this->locationName) + 1];
+			strcpy(copy, this->locationName);
+			return copy;
+		}
+	}
 	int get_no_of_rows() {
 		return this->no_of_rows;
 	}
@@ -21,8 +30,8 @@ public:
 		//if (this->no_of_seats == nullptr && this->no_of_rows == 0)
 			//return nullptr;
 
-		int *copy = new int[this->no_of_rows+1];	
-		for(int i = 0; i<this->no_of_rows; i++)
+		int* copy = new int[this->no_of_rows + 1];
+		for (int i = 0; i < this->no_of_rows; i++)
 			copy[i] = this->no_of_seats[i];
 		return copy;
 	}
@@ -32,6 +41,15 @@ public:
 	}
 
 	//setters with validations
+	void setLocName(const char* title) {
+		if (this->locationName != NULL)
+			delete[] this->locationName;
+
+		this->locationName = new char[strlen(title) + 1];
+		strcpy(this->locationName, title);
+
+	}
+
 	void set_no_of_rows(int value) {
 		if (value <= 10)
 			throw exception("Number of rows must be greater than 10. ");
@@ -40,7 +58,7 @@ public:
 	}
 
 	void set_no_of_seats(int* seats) {
-		if(this->no_of_seats != NULL)
+		if (this->no_of_seats != NULL)
 			delete[] this->no_of_seats;
 
 		this->no_of_seats = new int[no_of_rows + 1];
@@ -49,7 +67,7 @@ public:
 			this->no_of_seats[i] = seats[i];
 	}
 
-	void setExistenceofStand(bool is) {  
+	void setExistenceofStand(bool is) {
 		this->hasConcessionStand = is;
 	}
 
@@ -62,18 +80,29 @@ public:
 	}
 
 	//second generic method;
+	void searchforFood(const string foodName)
+	{
+		if (this->hasConcessionStand)
+			cout << endl << "We have a Concession Stand. Look there for " << foodName << " and see if there are any portions left.";
+		else
+			cout << endl << "Sorry we do not have a Concession Stand so we can't offer you any food or drink.";
+	}
 
 	//default construct
 	EventLocation()
 	{
+		this->locationName = nullptr;
 		this->no_of_rows = 0;
 		this->no_of_seats = nullptr;
 		this->hasConcessionStand = false;
 	}
 
 	//constructor with parameters
-	EventLocation(int no_of_rows, int* no_of_seats, bool hasConcessionStand)
+	EventLocation(const char* locationName, int no_of_rows, int* no_of_seats, bool hasConcessionStand)
 	{
+		this->locationName = new char[strlen(locationName) + 1];
+		strcpy(this->locationName, locationName);
+
 		this->no_of_rows = no_of_rows;
 
 		if (this->no_of_seats != NULL)
@@ -88,6 +117,12 @@ public:
 	//copy construct
 	EventLocation(const EventLocation& e)
 	{
+		if (e.locationName != NULL)
+		{
+		this->locationName = new char[strlen(e.locationName) + 1];
+		strcpy(this->locationName, e.locationName);
+		}
+
 		this->no_of_rows = e.no_of_rows;
 
 		if (e.no_of_seats != NULL)
@@ -102,14 +137,54 @@ public:
 		this->hasConcessionStand = e.hasConcessionStand;
 	}
 
+	//overloading operator =
+	EventLocation& operator=(const EventLocation& e) {
 
+		if (e.locationName != NULL)
+		{
+			if (this->locationName != NULL)
+			{
+				delete[] this->locationName;
+				this->locationName = nullptr;
+			}
+			this->locationName = new char[strlen(e.locationName) + 1];
+			strcpy(this->locationName, e.locationName);
+		}
+
+		this->no_of_rows = e.no_of_rows;
+
+		if (e.no_of_seats != NULL)
+		{
+			if (this->no_of_seats != NULL)
+				delete[] this->no_of_seats;
+			this->no_of_seats = new int[e.no_of_rows + 1];
+			for (int i = 0; i < e.no_of_rows; i++)
+				this->no_of_seats[i] = e.no_of_seats[i];
+		}
+
+		this->hasConcessionStand = e.hasConcessionStand;
+		return *this;
+	}
+
+	//overload operator--
+	void operator--() {
+		for (int i = 0; i < this->no_of_rows; i++)
+			if(this->no_of_seats[i] > 0)
+				this->no_of_seats[i] = this->no_of_seats[i] - 1;
+	}
+
+	//overload operator !
+	bool operator!() {
+		bool is = !hasConcessionStand;
+		return is;
+	}
 
 	friend istream& operator>>(istream& in, EventLocation& eventLocation);
 	friend ostream& operator<<(ostream& out, const EventLocation& eventLocation);
 
 	//destructor
 	~EventLocation() {
-		
+
 		if (this->no_of_seats != nullptr) {
 			delete[] this->no_of_seats;
 			this->no_of_seats = nullptr;
@@ -118,6 +193,14 @@ public:
 };
 
 istream& operator>>(istream& in, EventLocation& location) {
+
+	// Input for locationName
+	cout << "Enter Location Name: ";
+	char buffer[100];
+	in.ignore(); // Ignore newline left in the buffer
+	in.getline(buffer, 100);
+	location.setLocName(buffer);
+
 	// Input for number of rows
 	cout << "Enter Number of rows: ";
 	in >> location.no_of_rows;
@@ -141,17 +224,19 @@ istream& operator>>(istream& in, EventLocation& location) {
 }
 
 ostream& operator<<(ostream& out, const EventLocation& location) {
-	out <<"Number of rows: " << location.no_of_rows << endl;
+
+	out << "Location Name: " << location.locationName << endl;
+	out << "Number of rows: " << location.no_of_rows << endl;
 	out << "Number of reats per row: ";
 	for (int i = 0; i < location.no_of_rows; i++) {
 		out << location.no_of_seats[i] << " ";
 	}
-	out <<endl;
+	out << endl;
 	out << "Concession Stand: ";
 	if (location.hasConcessionStand)
-		out << "Yes"<<endl;
+		out << "Yes" << endl;
 	else
-		out << "No"<<endl;
+		out << "No" << endl;
 
 	return out;
 }
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -33,7 +33,7 @@ public:
 int main()
 {
 	Movie movie1("Inception", "23/09/2023", "12:30", 150, "sci-fi", "3D");
-	cout << "Title is: "<<movie1.getTitle() <<endl<< "Date: "<< movie1.getDate() <<endl<< "Time: "<< movie1.getTime()<<endl<< "Duration: " << movie1.getDuration()<<endl<<"Genre: " << movie1.getGenre()<<endl << "Format: "<< movie1.getFormat();
+	//cout << "Title is: "<<movie1.getTitle() <<endl<< "Date: "<< movie1.getDate() <<endl<< "Time: "<< movie1.getTime()<<endl<< "Duration: " << movie1.getDuration()<<endl<<"Genre: " << movie1.getGenre()<<endl << "Format: "<< movie1.getFormat();
 	Movie movie2(movie1);
 	cout <<endl<< "Movie2 Info: " <<endl<<"Title:"<< movie2.getTitle() << endl;
 
@@ -45,9 +45,29 @@ int main()
 	cout <<endl<<"New duration: "<< movie2.getDuration();
 	movie2.startMoment(20);
 
-	Movie movie3;
+	/*Movie movie3;
 	cin >> movie3;
-	cout << movie3;
+	cout << movie3;*/
 
-	EventLocation loc1;
+	int seatsPerRow[] = { 20, 25, 20, 25, 20, 25, 20, 25, 20, 25, 20, 25, 20, 25, 20, 25, 20, 25, 20, 25 };
+	EventLocation loc1("Afi Palace Cotroceni", 20, seatsPerRow, true);
+	int rows = loc1.get_no_of_rows();
+
+	//cout <<endl<< loc1;
+	loc1.searchforFood("Nachos");
+
+	if (!loc1) {
+		cout << endl<< "Unfortunately, there is no concession stand." << endl;
+	}
+	else {
+		cout <<endl<< "Yes. There is a concession stand." << endl;
+	}
+	
+	--loc1;
+
+	EventLocation loc2;
+	loc2 = loc1;
+
+	cout << endl << loc2;
+	
 }
