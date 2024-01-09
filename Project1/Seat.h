38ffb5eec59c17ab5a638497e86c4c2e4e52ee7b
Pavel Prisecariu <prisecariupavel22@stud.ase.ro>
2024-01-09 17:53:36 +0200

    Finished the Seat class definition

--- Project1/Seat.h
+++ Project1/Seat.h
@@ -52,9 +52,51 @@ public:
 			this->category = s.category;
 		return *this;
 	}
+
+	friend istream& operator>>(istream& in, Seat& seat);
+	friend ostream& operator<<(ostream& out, const Seat& seat);
 	
 	~Seat() {
 
 	}
 
 };
+
+istream& operator>>(istream& in, Seat& seat) {
+
+	cout << endl << "Enter the code of the seat: ";
+	in >> seat.seat_code;
+
+	cout << endl << "Is the seat available? Write 1 for yes and 0 for no. ";
+	in >> seat.availability;
+
+	cout << endl << "Specify category (0 for standard, 1 for VIP, 2 for disability): ";
+	int categoryInput;
+	in >> categoryInput;
+
+	if (categoryInput >= 0 && categoryInput <= 2) {
+		seat.category = static_cast<Category>(categoryInput);
+	}
+	else {
+		// Default to standard category if the provided value is invalid
+		cout << "Invalid category value. Defaulting to standard category." << endl;
+		seat.category = Category::standard;
+	}
+	return in;
+}
+
+ostream& operator<<(ostream& out, const Seat& seat) {
+	out << endl << "Seat Information: "<< endl;
+	out << "Seat_code: " << seat.seat_code << endl;
+	out << "Availability: " << seat.availability << endl;
+
+	if(seat.category == Category::standard)
+		out << "Category: Standard" << endl;
+	else if (seat.category == Category::VIP)
+		out << "Category: VIP" << endl;
+	else if (seat.category == Category::disability)
+		out << "Category: Disability" << endl;
+	//out << "Category: " << seat.category << endl;
+
+	return out;
+}
