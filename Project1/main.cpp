e564cb0dc37bd04c35902ff8c2989a0b94974050
Pavel Prisecariu <prisecariupavel22@stud.ase.ro>
2024-01-10 12:01:04 +0200

    Declared the text file

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -23,6 +23,23 @@ int main()
 	Concert concert1("Trap&Punk Madness", "25/04/2024", "21:00", 60, "Hvnds", "Punk/Trap");
 
 	StandUpShow show1("Neispiratie", "25/04/2024", "21:00", 60, 3, new string[3]{"Vio", "Mocanu", "Costel"}, "Micutzu");
+	show1.showUppercase();
 
+	char fileName[50];
+	//cout << "Enter file name (with extension): ";
+	//cin.getline(fileName, 50);
 
+	ifstream inputFile(fileName, ios::in);
+
+	//while (){
+	/*	int i, no_of_Objects;
+		file >> no_of_Objects;
+
+		for (i = 0; i < no_of_Objects; i++)
+		{
+
+		}
+	*/
+	//read title
+	
 }
