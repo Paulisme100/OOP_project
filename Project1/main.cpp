9cce4b11f88d66ab739218b3e451aed6b7dc8cd3
Pavel Prisecariu <prisecariupavel22@stud.ase.ro>
2024-02-08 12:04:23 +0200

    Created references for the input files; Read the locations for events from the text file; To have all locations in one structure Created an array of pointers to EventLocation, that is dinamically allocated When an object is read from the file, the address of its values is stored in an element of the locations array

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -35,20 +35,86 @@ int main()
 
 	}*/
 
-	ifstream inputFile("Data.txt", ios::in);
+	ifstream inputFile1("Locations.txt", ios::in);
+	ifstream inputFile2("Events.txt", ios::in);
 
-
-	EventLocation locatie;
-	if (!inputFile.is_open())
+	int no_of_loc;
+	if (!inputFile1.is_open())
 	{
 		cout << "***Error! The file can't be opened or is missing.";
 	}
 	else
 	{
-		locatie.readData(inputFile);
+		inputFile1 >> no_of_loc;
 	}
-	cout <<"Location Info: "<< locatie;
+	EventLocation** locations;
+	locations = new EventLocation* [no_of_loc];
 
-	inputFile.close();
-	
+	//for (int i = 0; i < no_of_loc; ++i) {
+		//locations[i] = new EventLocation;  // Allocate memory for an EventLocation and make locations[i] point to it
+	//}
+
+	if (!inputFile1.is_open())
+	{
+		cout << "***Error! The file can't be opened or is missing.";
+	}
+	else
+	{
+		for(int i = 0; i<no_of_loc; i++)
+		{
+			if (!inputFile1.is_open())
+			{
+				cout << "***Error! The file can't be opened or is missing.";
+			}
+			else
+			{
+				EventLocation loc;
+				loc.readData(inputFile1);
+				locations[i] = &loc;
+				//cout << locations[i]->getLocName()<<endl;
+				//locations[i]->printInfo();
+				cout << endl;
+			}
+		}
+	}
+
+	/*for (int i = 0; i < no_of_loc; i++)
+	{
+		cout << "Location " << i + 1 << " Info: " << endl;
+		locations[i]->printInfo();
+		cout << endl;
+	}*/
+
+	//EventLocation loc10;
+	//cin >> loc10;
+
+	/*StandUpShow show1;
+
+	if (!inputFile2.is_open())
+	{
+		cout << "***Error! The file can't be opened or is missing.";
+	}
+	else
+	{
+		show1.readEventData(inputFile2);
+	}
+
+	cout << endl <<  "Event Info: " << show1;
+	show1.printInfo();*/
+
+	inputFile1.close();
+	inputFile2.close();
+
+	/*Ticket tick1(3, 5, "Costi", "Bodega Ursilor", 30);
+	cout << endl << "Ticket Info: ";
+	cout << endl << tick1;
+
+	Ticket tick2;
+	tick1 = tick1-5;
+	*/
+	/*Event ev1("Concursul cu ursul", "25/03/2023", "18:10", 125);
+	cout << endl << "Event info: " << ev1;
+	ev1 = ev1 + 20;
+	cout << ev1;
+	*/
 }
